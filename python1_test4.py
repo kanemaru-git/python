30b6f297f901e3783b7d812298e43e107c28df14
kanemaru-git <https://github.com/kanemaru-git.com>
2020-07-02 15:43:06 +0900

    演算子の前後に空白、関係ないコードを削除

--- python1_test4.py
+++ python1_test4.py
@@ -1,19 +1,17 @@
-#借入金額
+# 借入金額
 debt = 250000
-#年利/12(月利)
-interest = 1+0.14/12
-#月々の返済金額
+# 年利/12(月利)
+interest = 1 + 0.14 / 12
+# 月々の返済金額
 repay = 30000
-#返済期間（月ごと）
+# 返済期間（月ごと）
 month = 0
 
-print(debt*interest-repay)
-
-while debt>repay:
-    debt = debt*interest-repay
+while debt > repay:
+    debt = debt * interest - repay
     month += 1
     print(month,"ヶ月目:返済額=",repay,"円,","残り",debt)
 else:
-    debt = debt*interest
+    debt = debt * interest
     month += 1
     print(month,"ヶ月目:返済額=",debt,"円,","返済完了")
