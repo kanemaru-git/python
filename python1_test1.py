8cd0a0e03e44c11957b5c31037ca425036895a87
kanemaru-git <https://github.com/kanemaru-git.com>
2020-07-02 15:38:27 +0900

    演算子の前後に空白を入れる

--- python1_test1.py
+++ python1_test1.py
@@ -1,7 +1,7 @@
 a = 11
 b = 3
 
-print("11 + 3 =",a+b)
-print("11 - 3 =",a-b)
-print("11 × 3 =",a*b)
-print("11 ÷ 3 =",a//b,"余り",a%b)
+print("11 + 3 =", a + b)
+print("11 - 3 =", a - b)
+print("11 × 3 =", a * b)
+print("11 ÷ 3 =", a // b,"余り", a % b)
