edc0b5cdc4fc5c9eafff3a0f19de761b20051be2
kanemaru-git <https://github.com/kanemaru-git.com>
2020-07-02 15:41:57 +0900

    演算子の前後に空白、変数名を小文字

--- python1_test3.py
+++ python1_test3.py
@@ -3,13 +3,13 @@ weight = int(input())
 print("身長を入力してください(cm)")
 height = int(input())
 
-BMI = weight/(height/100)**2
+bmi = weight / (height / 100) ** 2
 
-if BMI < 18.5:
+if bmi < 18.5:
     print("あなたは「やせ」です。")
-elif 18.5 <= BMI < 25:
+elif 18.5 <= bmi < 25:
     print("あなたは「標準」です。")
-elif 25 <= BMI <30:
+elif 25 <= bmi < 30:
     print("あなたは「肥満」です。")
 else:
     print("あなたは「高度肥満」です。")
