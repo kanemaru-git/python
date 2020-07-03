da07c3bfa019b25f88efad2df7ba49a14c57d8e7
kanemaru-git <https://github.com/kanemaru-git.com>
2020-07-03 13:49:42 +0900

    refs #3:インポート文を最初に修正、関数をひとまとまりに修正

--- python2_test2.py
+++ python2_test2.py
@@ -1,27 +1,23 @@
-# 閏年かどうか判定
-def isleap(year):
-    return year % 4 == 0 and (year % 100 != 0 or year % 400 == 0)
+# datetimeインポート
+import datetime
+
+# 閏年を判定する関数
+def isleap(year,year_name):
+    isleap = year % 4 == 0 and (year % 100 != 0 or year % 400 == 0)
+    if year_name == 0:
+        year_name = "今年"
+    elif year_name == -1:
+        year_name = "去年"
+    else:
+        year_name = "来年"
+    if isleap:
+        print("{}は閏年です。".format(year_name))
+    else:
+        print("{}は閏年ではありません。".format(year_name))
 
 # 現在日時を取得
-import datetime
 now = datetime.datetime.now()
 
-# 今年は閏年かどうか
-if isleap(now.year):
-    print("今年は閏年です。")
-else:
-    print("今年は閏年ではないです。")
-
-# 昨年は閏年かどうか
-if isleap(now.year - 1):
-    print("昨年は閏年です。")
-else:
-    print("昨年は閏年ではないです。")
-
-# 来年は閏年かどうか
-if isleap(now.year + 1):
-    print("来年は閏年です。")
-else:
-    print("来年は閏年ではないです。")
-
-# commitコメントにrefsつけ忘れたので再度pushしました。
+# 昨年から来年までの閏年を判定
+for year_name in range(-1,2):
+    isleap(now.year + year_name,year_name)
--- python2_test3.py
+++ python2_test3.py
@@ -1,20 +1,14 @@
-# 画像の高さ
-def height(image):
-    return image.height
-
-# 画像の横幅
-def width(image):
-    return image.width
-
-# 画像のアスペクト比
-def aspect(image):
-    return image.height / image.width
-
 # Pillowをインポート
 from PIL import Image, ImageFilter
+
+# 画像の情報を表示する関数
+def image_infomation(image):
+    print("画像の高さは{}pxです。".format(image.height))
+    print("画像の横幅は{}pxです。".format(image.width))
+    print("画像のアスペクト比は{:.2f}:1です。".format(image.width / image.height))
+
+# 画像を選択
 image = Image.open('../images/img.jpg')
 
-# 高さ、横幅、アスペクト比を出力
-print("画像の高さは",height(image),"pxです。")
-print("画像の横幅は",width(image),"pxです。")
-print("画像のアスペクト比は",aspect(image),"%です。")
+# 画像の情報を出力
+image_infomation(image)
