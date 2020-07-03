ebfbdfe7725db58dea5a8583eb893a02b86a8fc2
kanemaru-git <https://github.com/kanemaru-git.com>
2020-07-03 15:27:56 +0900

    refs #3:昨年から来年のlistを作成、閏年の判定条件を変更

--- python2_test2.py
+++ python2_test2.py
@@ -3,21 +3,20 @@ import datetime
 
 # 閏年を判定する関数
 def isleap(year,year_name):
-    isleap = year % 4 == 0 and (year % 100 != 0 or year % 400 == 0)
-    if year_name == 0:
-        year_name = "今年"
-    elif year_name == -1:
-        year_name = "去年"
+    if year % 100 == 0 and year % 400 != 0:
+        print("{}は閏年ではありません".format(year_name))
+    elif year % 4 == 0:
+        print("{}は閏年です".format(year_name))
     else:
-        year_name = "来年"
-    if isleap:
-        print("{}は閏年です。".format(year_name))
-    else:
-        print("{}は閏年ではありません。".format(year_name))
+        print("{}は閏年ではありません".format(year_name))
+
+# 昨年から来年までのリスト
+year_list = ["昨年","今年","来年"]
 
 # 現在日時を取得
 now = datetime.datetime.now()
 
 # 昨年から来年までの閏年を判定
-for year_name in range(-1,2):
-    isleap(now.year + year_name,year_name)
+for i,year_name in enumerate(year_list):
+    year = now.year - 1 + i
+    isleap(year,year_name)
