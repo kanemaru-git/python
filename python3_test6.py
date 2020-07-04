cc5954009813d14b223bb4f62b2c1e09699d5ded
kanemaru-git <https://github.com/kanemaru-git.com>
2020-07-04 15:07:53 +0900

    refs #5:python3_test6課題提出

---
+++ python3_test6.py
@@ -0,0 +1,13 @@
+# インポート
+import python3_test5
+
+# インスタンス
+smartphone = python3_test5.SmartPhone("電話番号","メールアドレス")
+
+# メソッド呼び出し
+smartphone.play_music()
+smartphone.next_song()
+smartphone.previous_song()
+smartphone.stop_music()
+smartphone.call()
+smartphone.send_mail()
