9b5041c38a317bcd4b28c16f6c65b5dde652fe52
kanemaru-git <https://github.com/kanemaru-git.com>
2020-07-04 13:48:08 +0900

    refs #5:python3_test1課題提出

---
+++ python3_test1.py
@@ -0,0 +1,14 @@
+# MP3Playerクラス
+class MP3Player:
+
+    def play_music(self):
+        print("音楽を再生します")
+    
+    def next_song(self):
+        print("次の曲を再生します")
+    
+    def previous_song(self):
+        print("前の曲を再生します")
+    
+    def stop_music(self):
+        print("音楽を止めます")
